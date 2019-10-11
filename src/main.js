29f121eb32fb6ce91811c6510f25d54cf35611b1
Angel Briceno <angelb1108@gmail.com>
2019-10-10 22:54:38 -0400

    vue + bootstrap

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/*
+ * BootstrapVue
+ */
+
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
